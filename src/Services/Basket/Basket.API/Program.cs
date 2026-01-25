f49cec990d98dc543bfe23de6905708303d93864
lleratodev <lleratodev@gmail.com>
2026-01-26 00:39:05 +0200

    feature: add basketdb to docker compose
    - basket api program config
    - add postgres db for basketdb on 5433 port

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,10 +1,47 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container (DI)
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddCarter();
+
 var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+#region Database Configuration
+var connectionString = builder.Configuration.GetConnectionString("Database");
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+#endregion
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString!);
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
