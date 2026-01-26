2a81d0ad55245268cdb1a3f347ab85f005339b55
lleratodev <lleratodev@gmail.com>
2026-01-26 13:14:42 +0200

    feature: discount service coupons migrations

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountDbContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountDbContext(DbContextOptions<DiscountDbContext> options)
+            : base(options)
+        { }
+
+
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,8 +1,15 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DiscountDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
