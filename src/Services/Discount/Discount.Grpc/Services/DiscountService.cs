ac6a47f7534254c2c20013ff06e516564104e3da
lleratodev <lleratodev@gmail.com>
2026-01-26 13:15:00 +0200

    feature: handlers for protobuf calls

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -3,11 +3,31 @@ using Grpc.Core;
 
 namespace Discount.Grpc.Services;
 
-public class DiscountService
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
 {
     private readonly ILogger<DiscountService> _logger;
     public DiscountService(ILogger<DiscountService> logger)
     {
         _logger = logger;
+    }
+
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
     }
 }
